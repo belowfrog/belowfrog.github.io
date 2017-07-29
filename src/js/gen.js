74e683bdc2b78936546e5858cbf39787d6a72b4d
belowfrog <belowfrog@gmail.com>
2017-07-29 21:20:15 +0800

    remove gen

--- src/js/gen.js
+++ src/js/gen.js
@@ -1,23 +1,6 @@
 import crypto from 'crypto';
 
-const concatParams = (params) => {
-  return params.map(({key, value})=>key + '=' + value).join('&');
-};
-
-const genSig = (params, consumerSecret)=> {
-  let str = concatParams(params) + consumerSecret;
-  return crypto.createHash('sha1').update(new Buffer(str).toString('hex')).digest('hex');
-};
-
 const buildUrl = (oid, prefix, consumerSecret) => {
-  const params = [
-    {key: 'oid', value: oid},
-    {key: 'ts', value: (new Date()).getTime().toString()}
-  ];
-  const query = concatParams(params.concat({
-    key: 'sig',
-    value: genSig(params, consumerSecret),
-  }));
   return `${prefix}${query}&bizOrderId=12313`;
 }
 
